f72d568d8721c956a2a5058c46f338ff81d39aaf
shreyanshgupta42 <57253725+shreyanshgupta42@users.noreply.github.com>
2021-05-20 00:18:44 +0530

    added react states and react onclick event

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';               // this useState is the react states
 import Square from './Square';
 
 const Board = () => {
+  const [board, setboard] = useState(Array(9).fill(null)); //  we have here two values because useState returns two values , board is the current position and set board is going to be the next position
+  const [isXNext, setisXNext] = useState(false);
+
+  const handleSquareClick = (position) => {
+    if(board[position]){
+      return;
+    }
+    setboard( prev =>{
+      return prev.map((square,pos) => {
+        if(pos===position){
+          return isXNext ? 'X' : 'O';
+        }
+        return square;
+      })
+      
+    })
+    setisXNext((prev)=> !prev);
+  };
+
+  const renderSquare = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}             // this onClick is reacts events
+      />
+    );
+  };
+
   return (
     <div className="board">
-      <div className='board-row'>
-        <Square value={0}/>
-        <Square value={1}/>
-        <Square value={2}/>
+      <div className="board-row">
+        {renderSquare(0)}                                      
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-      <div className='board-row'>
-        <Square value={3}/>
-        <Square value={4}/>
-        <Square value={5}/>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
-      <div className='board-row'>
-        <Square value={6}/>
-        <Square value={7}/>
-        <Square value={8}/>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
